d8fd2fb67bd70f45798e6cfc67c86c53727dc61f
owells <oliver_wells@sfu.ca>
2020-06-10 17:35:45 +0300

    Added createChamber method into Level.java class
    I created this chamber surrounding the 20x15 matrix with walls. This enables us to work within a chamber as the assignment requires.
    In order to work within this chamber, if using the same map Array from Tile.java class we need to start Width index from 1 and Height index from 1 and set their respective sizes within loops or recursion as 18 and 13. Thus we will treat the 20x15 matrix as a 18x13 matrix while implementing the maze algorithm.

--- src/ca/sfu/cmpt213/assignment2/ui/Level.java
+++ src/ca/sfu/cmpt213/assignment2/ui/Level.java
@@ -12,7 +12,26 @@ public class Level {
     private static final int HEIGHT = 15;
     private Tile[][] map = new Tile[WIDTH][HEIGHT];
 
+    //we can actually ommit the Tile.Terrain.wall or empty by just moving the enum to a seperate file....
+    private void createChamber() {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (i == 0)
+                    map[i][j].setTerrain(Tile.Terrain.wall);
+                else if (j == 0) {
+                    map[i][j].setTerrain(Tile.Terrain.wall);
+                } else if (j == 14 || i == 19) {
+                    map[i][j].setTerrain(Tile.Terrain.wall);
+                } else
+                    map[i][j].setTerrain(Tile.Terrain.empty);
+            }
+
+        }
+
+    }
+
     public Tile[][] getMap() {
+
         return map;
     }
 
